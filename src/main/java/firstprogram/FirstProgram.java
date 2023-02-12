6194636a00977b8d8fb0ea2bf8339164f97b0e41
Pawel Wojcik <pawel.wojcik@intive.com>
2023-02-12 10:58:43 +0100

    additional comments

--- src/main/java/firstprogram/FirstProgram.java
+++ src/main/java/firstprogram/FirstProgram.java
@@ -5,8 +5,11 @@ package firstprogram;
 public class FirstProgram {
     //początek programu - metoda main
     public static void main(String[] args) {
-
+        //wiadomość którą chcecie wypisać na ekran
         System.out.println("Witaj świecie!!!");
-
+        /*
+         co się wydarzyło w momecie odpalenia programu
+           -kod z klasy FirstProgram.java został przetłumaczony na FirstProgram.class - kod bajtowy
+        * */
     }
 }
