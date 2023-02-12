c58a98957b30bca61f1f86145811960bc276de64
Pawel Wojcik <pawel.wojcik@intive.com>
2023-02-12 10:56:36 +0100

    Initial commit

---
+++ src/main/java/firstprogram/FirstProgram.java
@@ -0,0 +1,12 @@
+package firstprogram;
+
+//konwencja nazewnictwa: z dużej litery i camel case
+//plik w którym piszemy kod źródłowy (kod programu)
+public class FirstProgram {
+    //początek programu - metoda main
+    public static void main(String[] args) {
+
+        System.out.println("Witaj świecie!!!");
+
+    }
+}
