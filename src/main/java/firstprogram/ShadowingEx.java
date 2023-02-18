80d85c7fcc0f7d41156e41445e1e0f2264414c0b
Pawel Wojcik <pawel.wojcik@intive.com>
2023-02-18 11:52:07 +0100

    shadowing

--- src/main/java/firstprogram/ShadowingEx.java
+++ src/main/java/firstprogram/ShadowingEx.java
@@ -2,10 +2,25 @@ package firstprogram;
 
 //przysłanianie - zmienna zadeklarowana w lokalnie "przysłania" zmienną zadeklarowaną lokalnie
 public class ShadowingEx {
-    static int age;
+    // można tworzyć zmienne bezpośrednio po nazwie klasy
+    //takie zmienne nazywają się zmiennymi globalnymi
+    //tworząc zmienną tutaj, ona jest widoczna w całej metodzie main i w całej klasie
+    static int age; // globalna zmienna age
+
     public static void main(String[] args) {
-      age = 2;
-      int age =3;
+        age = 2; // zmienne tworzone wewnątrz metod to są zmienne lokalne
+
+        int age = 3;
+        //można stworzyć zmienną lokalną wewnątrz bloku, która będzie nazywała się tak samo jak
+        //zmienna globalna. W takiej sytuacji posługując sie nazwą tej zmiennej, wewnątrz tego bloku
+        // będziemy mieli wartość zmiennej lokalnej // shadowing - przesłanianie
+        // zostanie wypisane 3
+        System.out.println(age);
+    }
+
+    public static void myMethod(String[] args) {
+        age = 2;
+        int age = 3;
         System.out.println(age);
     }
 }
