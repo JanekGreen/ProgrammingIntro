7dc772d51d7161582f8d5472398c60bc2e34e40b
Pawel Wojcik <pawel.wojcik@intive.com>
2023-02-18 11:09:43 +0100

    Switch examples

--- src/main/java/firstprogram/SwitchStatement.java
+++ src/main/java/firstprogram/SwitchStatement.java
@@ -9,9 +9,18 @@ public class SwitchStatement {
         // różnica pomiędzy wyrażeniem a instrukcją (statement vs expression)
         int day = scanner.nextInt();
         scanner.nextLine();
+        //switch porównuje wartości do poszczególnych case'ów, w przypadku gdy trafi na pasujący case, nie sprawdza kolejnych
+        //można używać dowolnej zmiennej prymitywnej, czyli:
+        /*
+        typy proste: byte, short, char, int
+        odpowiadające im klasy opakowujące: Byte, Short, Character, Integer
+        String
+        enum
+        * */
         switch (day) {
             case 1:
                 System.out.println("Poniedzialek.");
+                //może być tutaj więcej kodu
                 break;
             case 2:
                 System.out.println("Wtorek.");
@@ -33,6 +42,30 @@ public class SwitchStatement {
                 break;
             default:
                 System.out.println("Nieznany dzien tygodnia!");
+        }
+        // nowa wersja switcha od Javy 11
+        switch (day) {
+            case 1 -> System.out.println("Poniedzialek.");
+            case 2 -> System.out.println("Wtorek.");
+            case 3 -> System.out.println("Sroda.");
+            case 4 -> System.out.println("Czwartek.");
+            case 5 -> System.out.println("Piatek.");
+            case 6 -> System.out.println("Sobota.");
+            case 7 -> System.out.println("Niedziela.");
+            default -> System.out.println("Nieznany dzien tygodnia!");
+        }
+        String name ="Jan";
+        switch (name){
+            case "Jan":
+                System.out.println("Cześć Janek");
+                break;
+            case "Magda":
+                System.out.println("Hej Magda");
+                break;
+            default: // default to jest taki else, zostanie dopasowany do każdej innej wartości
+                    // niż te zdefiniowane powyzej. Występuje zawsze na końcu
+                    //może go nie być
+                System.out.println("Nie znam Cię");
         }
 
     }
