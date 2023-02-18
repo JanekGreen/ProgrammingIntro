02d97218784a48e15b58372c956a5d5c4958e287
Pawel Wojcik <pawel.wojcik@intive.com>
2023-02-18 08:33:25 +0100

    wrapper classes

---
+++ src/main/java/firstprogram/WrapperClasses.java
@@ -0,0 +1,14 @@
+package firstprogram;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Byte b = 2;
+        Short s = 32000;
+        Integer myInt = 5;
+        Long lng = Long.MAX_VALUE;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        Boolean bool = true;
+        Float fl = 1.6f;
+    }
+}
